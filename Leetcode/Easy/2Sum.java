39156b74bd0c89a5d980531e68390912a364d9db
Michael Aizenshtadt <Michael@Michaels-MacBook-Pro.local>
2021-09-24 17:50:22 -0400

    changed 2Sum

--- Leetcode/Easy/2Sum.java
+++ Leetcode/Easy/2Sum.java
@@ -1,24 +1,15 @@
 import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int index1 = 0;
-        int index2 = 0;
-        HashMap<Integer, Integer> differences = new HashMap<Integer, Integer>();
-        
-        for(int i = 0; i < nums.length; i++){
-            differences.put(nums[i], i);
-        }
-        
-        for (int i=0; i < nums.length; i++){
-            if(differences.containsKey(target-nums[i])){
-                index1 = i;
-                index2 = differences.get(target-nums[i]);
-                if(index1 != index2){
-                   return new int[] {index1, index2}; 
-                }
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                return new int[] {map.get(target-nums[i]), i};
+            }
+            else{
+                map.put((nums[i]), i);
             }
         }
-        
         return new int[] {};
     }
 }
